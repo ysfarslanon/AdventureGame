6cb7ae51f79b6fd1ea9a7c38a6e6f7c39648b4b4
Yusuf ARSLAN <arslanon@outlook.com>
2021-09-09 18:56:28 +0300

    Inventory

--- src/Source/Inventory.java
+++ src/Source/Inventory.java
@@ -1,2 +1,30 @@
-package Source;public class Inventory {
+package Source;
+
+import Items.Armor;
+import Items.Weapon;
+
+public class Inventory {
+    private Weapon weapon;
+    private Armor armor;
+
+    public Inventory() {
+        this.weapon = new Weapon(-1,"Yumruk",0,0);
+        this.armor = new Armor(-1,"Geleneksel kıyafet",0,0);
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public Armor getArmor() {
+        return armor;
+    }
+
+    public void setArmor(Armor armor) {
+        this.armor = armor;
+    }
 }
