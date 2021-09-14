898c56d5a45a4c3c3db5d3c059a42bfb145096c9
Yusuf ARSLAN <arslanon@outlook.com>
2021-09-14 22:23:09 +0300

    win locations award 2

--- src/Source/Inventory.java
+++ src/Source/Inventory.java
@@ -6,10 +6,16 @@ import Items.Weapon;
 public class Inventory {
     private Weapon weapon;
     private Armor armor;
+    private boolean isFood;
+    private boolean isFirewood;
+    private boolean isWater;
 
     public Inventory() {
         this.weapon = new Weapon(-1,"Yumruk",0,0);
         this.armor = new Armor(-1,"Geleneksel kıyafet",0,0);
+        this.isFood=false;// Mağara ödülü
+        this.isFirewood=false;// Orman ödülü
+        this.isWater=false;// Nehir ödülü
     }
 
     public Weapon getWeapon() {
@@ -26,5 +32,29 @@ public class Inventory {
 
     public void setArmor(Armor armor) {
         this.armor = armor;
+    }
+
+    public boolean isFood() {
+        return isFood;
+    }
+
+    public void setFood(boolean food) {
+        isFood = food;
+    }
+
+    public boolean isFirewood() {
+        return isFirewood;
+    }
+
+    public void setFirewood(boolean firewood) {
+        isFirewood = firewood;
+    }
+
+    public boolean isWater() {
+        return isWater;
+    }
+
+    public void setWater(boolean water) {
+        isWater = water;
     }
 }
