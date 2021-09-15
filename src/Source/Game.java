d41be483ba03d5fc9e0dbe38df87b0ec57420813
Yusuf ARSLAN <arslanon@outlook.com>
2021-09-15 23:14:38 +0300

    add login() and menu() function | Game

--- src/Source/Game.java
+++ src/Source/Game.java
@@ -6,28 +6,34 @@ import Locations.BattleLocations.River;
 import Locations.Location;
 import Locations.SafeLocations.SafeHouse;
 import Locations.SafeLocations.Store;
-
 import java.util.Scanner;
 
 public  class Game {
     private static Scanner input=new Scanner(System.in);
-    public static void start(){
-        System.out.println("Metin tabanlı macera oyununa hoşgeldin");
+
+    public static String login(){
+        System.out.println("\nMetin tabanlı macera oyununa hoşgeldin");
         System.out.print("Lütfen sana nasıl hitap edeceğimi söylermisin? : ");
         String nickname= "yusuf";//input.nextLine();
         System.out.println("\nSelam "+nickname+". Hadi devam edelim. Şimdi bir karakter seçmen gerekiyor");
-        Player player=new Player(nickname);
+        return nickname;
+    }
+
+    public static void menu(){
+        System.out.println("\n########### Mekanlar ###########");
+        System.out.println("0 - Gerçek dünya");
+        System.out.println("1 - Güvenli ev (Burada yaralar sarılır ve iyileşirsin.)");
+        System.out.println("2 - Mağaza (Burada kendini geliştirebileceğin aletler bulabilirsin.)");
+        System.out.println("3 - Mağara (Zombilerin yer aldığını ıssız ve karanlık mekan. ÖDÜL:YEMEK)");
+        System.out.println("4 - Orman (Vampirlerin dolaştığını mekan. ÖDÜL:ODUN)");
+        System.out.println("5 - Nehir (Ayıların koruduğu kutsal bir mekan. ÖDÜL:SU)");
+    }
+    public static void start(){
+        Player player=new Player(login());
         player.selectCharacter();
         Location location=null;
         while(true){
-            System.out.println("Food: "+player.getInventory().isFood()+" Firewood: "+player.getInventory().isFirewood()+" Water: "+player.getInventory().isWater());
-            System.out.println("\n########### Mekanlar ###########");
-            System.out.println("0 - Gerçek dünya");
-            System.out.println("1 - Güvenli ev (Burada yaralar sarılır ve iyileşirsin.)");
-            System.out.println("2 - Mağaza (Burada kendini geliştirebileceğin aletler bulabilirsin.)");
-            System.out.println("3 - Mağara (Zombilerin yer aldığını ıssız ve karanlık mekan. ÖDÜL:YEMEK)");
-            System.out.println("4 - Orman (Vampirlerin dolaştığını mekan. ÖDÜL:ODUN)");
-            System.out.println("5 - Nehir (Ayıların koruduğu kutsal bir mekan. ÖDÜL:SU)");
+            menu();
             System.out.print("Yolculuk nereye: ");
             int selectLocation=input.nextInt();
             while (selectLocation<0 || selectLocation>6){
@@ -56,11 +62,11 @@ public  class Game {
                 System.out.println("Tekrar görüşmek üzere");
                 break;
             }
-
             if (!location.onLocation()){
                 System.out.println("Öldün");
                 break;
             }
-        }
-    }
+
+        }// WHİLE
+    }// START
 }
