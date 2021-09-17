e47f542daea57d0a86d21f328a049fb91b59c087
Yusuf ARSLAN <arslanon@outlook.com>
2021-09-17 23:34:24 +0300

    new enemies

--- src/Enemies/Snake.java
+++ src/Enemies/Snake.java
@@ -1,4 +1,13 @@
 package Enemies;
 
-public class Snake {
+import java.util.Random;
+
+public class Snake extends Enemy{
+    private Random random=new Random();
+    private int damage;
+    public Snake() {
+        super(4, "Yılan", 12,new Random().nextInt(4)+3, 0);
+    }
+
+
 }
